11f0fcbfac0bac514ee65d6d59a7e52c107baff8
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-15 09:03:30 +0530

    Initial setup for React project

---
+++ gossapp-front-end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! This is Gossapp here...in its initial phase</h1>
+    </div>
+  );
+}
+
+export default App;
