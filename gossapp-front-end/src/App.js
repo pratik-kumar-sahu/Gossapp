31df55ea5cf1cd0402a586f4415a24e60533f4ed
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-17 21:27:50 +0530

    added login component and setup google sign in

--- gossapp-front-end/src/App.js
+++ gossapp-front-end/src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Chat from "./component/Chat/Chat";
+import Login from "./component/Login/Login";
 import Sidebar from "./component/Sidebar/Sidebar";
 
 function App() {
-  return (
+  const [user, setUser] = useState(null);
+
+  return !user ? (
+    <Login />
+  ) : (
     <div className="app">
       <Router>
         <Sidebar />
---
+++ gossapp-front-end/src/component/Login/Login.js
@@ -0,0 +1,30 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import { auth, provider } from "../../firebase";
+import "./Login.scss";
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((e) => alert(e.message));
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          alt="Whatsapp svg"
+        />
+        <div className="login__text">
+          <h1>Sign in to Whatsapp</h1>
+        </div>
+
+        <Button onClick={signIn}>Sign in with google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
